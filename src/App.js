f129bf7c19612a7e3050e2bc75b3bcee3d6492c3
Younghee <kwak.younghee@gmail.com>
2023-02-09 01:18:31 -0800

    feat: input fields for satellite and dates

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import { db } from "./firebase-config";
-import { collection, doc, getDocs } from "firebase/firestore";
+// import { db } from "./firebase-config";
+// import { collection, doc, getDocs } from "firebase/firestore";
 
 function App() {
   const [users, setUsers] = useState([]);
-  const usersCollectionRef = collection(db, "users");
+  //const usersCollectionRef = collection(db, "users");
 
-  useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(usersCollectionRef);
-      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
+  // useEffect(() => {
+  //   const getUsers = async () => {
+  //     const data = await getDocs(usersCollectionRef);
+  //     setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  //   };
 
-    getUsers();
-  }, []);
+  //   getUsers();
+  // }, []);
 
   const [formData, setFormData] = useState([]);
   const handleChange = (event) => {
@@ -38,7 +38,16 @@ function App() {
   const makeScript = (formData) => {
     const latitude = formData["latitude"];
     const longitude = formData["longitude"];
+    const satellite = formData["satellite"];
+    const startDate = formData["startDate"];
+    const endDate = formData["endDate"];
+    const collection = "LANDSAT/LC08/C02/T1_TOA";
     const script = `
+    var l8 = ee.ImageCollection(${collection});
+    ${satellite}
+    ${startDate}
+    ${endDate}
+    var median = l8.filterDate('2016-01-01', '2016-12-31').median();
     var point = ee.Geometry.Point(${latitude}, ${longitude});
     var pointCoordinates = point.coordinates();
     print('point.coordinates(...) =', pointCoordinates);
@@ -57,18 +66,40 @@ function App() {
       </header>
       <div>
         <form onSubmit={handleNewFormSubmit}>
-          <input
-            type="number"
-            name="latitude"
-            onChange={handleChange}
-            placeholder="Latitude..."
-          />
-          <input
-            type="number"
-            name="longitude"
-            onChange={handleChange}
-            placeholder="Longitude..."
-          />
+          <p align="left">
+            <label htmlFor="latitude">Location:</label>
+            &nbsp;
+            <input
+              type="number"
+              name="latitude"
+              onChange={handleChange}
+              placeholder="Latitude..."
+            />
+            &nbsp; &nbsp;
+            <input
+              type="number"
+              name="longitude"
+              onChange={handleChange}
+              placeholder="Longitude..."
+            />
+          </p>
+          <p align="left">
+            <label htmlFor="satellite">Satellite:</label>
+            &nbsp;
+            <input
+              type="text"
+              name="satellite"
+              onChange={handleChange}
+              placeholder="Satellite..."
+            />
+          </p>
+          <p align="left">
+            <label htmlFor="dataDate">Date:</label>
+            &nbsp;
+            <input type="date" id="startDate" name="startDate"></input>
+            &nbsp; &nbsp;
+            <input type="date" id="endDate" name="endDate"></input>
+          </p>
           <input type="submit" value="Make Script" />
         </form>
       </div>
