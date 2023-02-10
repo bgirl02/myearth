1372f5ba75ede2b2b4cd994e1c371baf153bc72f
Younghee <kwak.younghee@gmail.com>
2023-02-10 14:50:37 -0800

    style & clean: box styling & links to new tabs & remove unused part

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
-// import { db } from "./firebase-config";
-// import { collection, doc, getDocs } from "firebase/firestore";
 
 function App() {
-  const [users, setUsers] = useState([]);
-  //const usersCollectionRef = collection(db, "users");
-
-  // useEffect(() => {
-  //   const getUsers = async () => {
-  //     const data = await getDocs(usersCollectionRef);
-  //     setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-  //   };
-
-  //   getUsers();
-  // }, []);
-
   const [formData, setFormData] = useState([]);
   const handleChange = (event) => {
     console.log(
@@ -41,11 +27,10 @@ function App() {
     const satellite = formData["satellite"];
     const startDate = formData["startDate"];
     const endDate = formData["endDate"];
-    const collection = "LANDSAT/LC08/C02/T1_TOA";
     const script = `
-    var l8 = ee.ImageCollection('${satellite}');
-    var median = l8.filterDate('${startDate}', '${endDate}').median();
-    Map.setCenter(${longitude}, ${latitude});
+    var dataset = ee.ImageCollection('${satellite}');
+    var median = dataset.filterDate('${startDate}', '${endDate}').median();
+    Map.setCenter(${longitude}, ${latitude}, 8);
     Map.addLayer(median);
     `;
     console.log(script);
@@ -54,23 +39,25 @@ function App() {
 
   return (
     <div className="App">
-      <header>
-        <nav class="flex-container">
-          <h1 class="flex-item2">
-            <a href="https://earthengine.google.com/">
-              <img
-                alt="link to Google Earth Engine"
-                src="../geelogo.png"
-                width="40"
-                height="38"
-              ></img>
-            </a>
-          </h1>
-          <h1 class="flex-title">My Earth</h1>
-          <h1 class="flex-item1">sign in</h1>
-        </nav>
+      <header class="flex-container">
+        <h1 class="flex-item2">
+          <a
+            href="https://earthengine.google.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              alt="link to Google Earth Engine"
+              src="../geelogo.png"
+              width="40"
+              height="38"
+            ></img>
+          </a>
+        </h1>
+        <h1 class="flex-title">My Earth</h1>
+        <h1 class="flex-item1">sign in</h1>
       </header>
-      <div>
+      <div class="script-input">
         <form onSubmit={handleNewFormSubmit} id="scriptform">
           <p align="left">
             <label htmlFor="latitude">Location:</label>
@@ -100,6 +87,7 @@ function App() {
             >
               <option value="LANDSAT/LC08/C02/T1_TOA">LANDSAT</option>
               <option value="COPERNICUS/S2_SR_HARMONIZED">Sentinel</option>
+              <option value="MODIS/061/MCD43A4">MODIS</option>
             </select>
           </p>
           <p align="left">
@@ -124,9 +112,20 @@ function App() {
           </p>
         </form>
       </div>
-      <div>
+      <div class="script-result">
         <h3 align="left">Google Earth Engine snippet</h3>
         <pre align="left">{script}</pre>
+      </div>
+      <div class="map-result">
+        <h3 align="left">
+          <a
+            href="https://code.earthengine.google.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Earth Look
+          </a>
+        </h3>
       </div>
     </div>
   );
